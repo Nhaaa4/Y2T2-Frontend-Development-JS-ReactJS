7555b39b8061b82b2d2cbf6d6aea7065371a7276
Nhaaa4 <panahsovan51@gmail.com>
2025-01-16 22:28:36 +0700

    update

--- Week 1/S2 - PRACTICE/1 - My code/challenge2.js
+++ Week 1/S2 - PRACTICE/1 - My code/challenge2.js
@@ -10,6 +10,10 @@ var array1 = [14, 15, 16, 20];
 var array2 = [5, 4, 3, 2, 1];
 var array3 = [];
 
-console.log(challenge2(array1));
-console.log(challenge2(array2));
-console.log(challenge2(array3));
+array1 = challenge2(array1);
+array2 = challenge2(array2);
+array3 = challenge2(array3);
+
+console.log(array1);
+console.log(array2);
+console.log(array3);
